da182dbcd9fc108f81307408205682c0e4ff0142
Zhexin Zhang <zhexin@email.sc.edu>
2019-04-09 14:28:47 -0400

    search bar function should change category seach to course search
    Currently as the course has not been implemented. So I just use the category to show the outlook of this page.

--- findTutor/src/app/student-explore/student-explore.page.ts
+++ findTutor/src/app/student-explore/student-explore.page.ts
@@ -5,6 +5,8 @@ import {AlertController} from '@ionic/angular';
 import {IonicStorageModule} from '@ionic/Storage';
 import {Storage} from '@ionic/Storage';
 import { ItemService } from '../item.service';
+import { NavController } from '@ionic/angular';
+import { filter } from 'rxjs/operators';
 
 import { Observable } from 'rxjs';
 
@@ -14,13 +16,18 @@ import { Observable } from 'rxjs';
   styleUrls: ['./student-explore.page.scss'],
 })
 export class StudentExplorePage implements OnInit {
+    public categoryList:Array<any>;
+    public loadedcategoryList:Array<any>;
+    public categoryRef: firebase.database.Reference;
   categoryObervable: Observable<any[]>;
   categories: Array<any> = [];
   isStudent = false;
   rowList: Array<any> = [];
+  searchKey = "";
   constructor(
       private router: Router,
-      public itemService: ItemService)
+      public itemService: ItemService,
+      public navCtrl: NavController)
   {
     console.log('explore page constructed');
     console.log( "usertype:" + this.itemService.usertype);
@@ -30,8 +37,48 @@ export class StudentExplorePage implements OnInit {
     else{
       this.isStudent = false;
     }
-  }
+      this.categoryRef = firebase.database().ref('/categories');
 
+      this.categoryRef.on('value', categoryList => {
+          let Catgories = [];
+          categoryList.forEach( category => {
+              Catgories.push(category.val());
+              return false;
+          });
+
+          this.categoryList = Catgories;
+          this.loadedcategoryList = Catgories;
+      });
+  }
+    initializeItems(){
+        this.categoryList = this.loadedcategoryList;
+    }
+
+    getItems(searchbar) {
+        // Reset items back to all of the items
+        this.initializeItems();
+
+        // set q to the value of the searchbar
+        var q = searchbar.srcElement.value;
+        console.log('q: ' + q);
+
+        // if the value is an empty string don't filter the items
+        if (!q) {
+            return;
+        }
+        console.log('categoryList size: ' + this.categories.length);
+        this.categoryList = this.categories.filter((v) => {
+            if(v.name && q) {
+                if (v.name.toLowerCase().indexOf(q.toLowerCase()) > -1) {
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        console.log(q, this.categoryList.length);
+
+    }
   ngOnInit() {
     this.categoryObervable = this.itemService.getCategories();
     console.log('imported categories');
@@ -40,6 +87,8 @@ export class StudentExplorePage implements OnInit {
       if(this.categories != undefined) {
         console.log('There are ' + this.categories.length + ' categories in explore page.');
         console.log('There are ' + this.categories.length + ' to be iterated');
+         this.rowList=[];
+         this.rowList=this.getRowListByGridList(3);
       }
     })
 
