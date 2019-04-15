397c5c0c2a92cfecef0b41227be3a8df592af61c
oliveydeng@gmail.com <29358427+oliveDY@users.noreply.github.com>
2019-04-15 17:49:19 -0400

    Add "Student Couse Detail Page"

---
+++ findTutor/src/app/student-course-detail/student-course-detail.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-course-detail',
+  templateUrl: './student-course-detail.page.html',
+  styleUrls: ['./student-course-detail.page.scss'],
+})
+export class StudentCourseDetailPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
