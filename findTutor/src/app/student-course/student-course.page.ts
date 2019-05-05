9cea6e2f24833339f2dd6760e77f14ae2607e406
Yuan Huang <hyuan2011@gmail.com>
2019-05-05 14:22:32 -0400

    Fix the ngonit

--- findTutor/src/app/student-course/student-course.page.ts
+++ findTutor/src/app/student-course/student-course.page.ts
@@ -10,9 +10,9 @@ import { Events } from '@ionic/angular';
   styleUrls: ['./student-course.page.scss'],
 })
 export class StudentCoursePage implements OnInit {
- 
-  student_courses=[
-  ];
+  ordersdb = firebase.database().ref('orders/');
+  student_courses: Array<any> =[];
+  //student_courses=[];
   constructor(
     private router: Router,
     public itemService : ItemService,
@@ -23,21 +23,29 @@ export class StudentCoursePage implements OnInit {
   current_user = firebase.auth().currentUser;
 
   ngOnInit() {
+    this.ordersdb.on('value', resp => {
+      this.student_courses = [];
+      this.student_courses = snapshotToArray_StudentCouse(resp);
+      console.log(this.student_courses.length+" courses loaded");
+      console.log(this.student_courses);
+
+      this.events.publish('dataloaded',Date.now());
+    });
     // console.log("ngOnInit ...");
     // console.log("courses no: "+this.student_courses.length)
     // if(this.student_courses  != undefined){
     //       console.log("couse length" + this.student_courses .length);
     // }
-    var self = this;
-    this.events.subscribe('dataloaded', (time) => {
-      // user and time are the same arguments passed in `events.publish(user, time)`
-      console.log('data load time:', time);
-      self.student_courses = this.itemService.getStudentCourses();
-      console.log("courses loaded: " + self.student_courses);
-      });
+    // var self = this;
+    // this.events.subscribe('dataloaded', (time) => {
+    //   // user and time are the same arguments passed in `events.publish(user, time)`
+    //   console.log('data load time:', time);
+    //   self.student_courses = this.itemService.getStudentCourses();
+    //   console.log("courses loaded: " + self.student_courses);
+    //   });
 
-      // load student course when construct the page
-      this.student_courses  = this.itemService.getStudentCourses();
+    //   // load student course when construct the page
+    //   this.student_courses  = this.itemService.getStudentCourses();
   }
   hideOrNot(value){
     if(value==="pending"|| value==="deny" || value==="s_rated"|| value==="rated")
@@ -53,4 +61,20 @@ export class StudentCoursePage implements OnInit {
     this.router.navigate(["/student-rate-tutor", course]);
   }
 
+}
+
+export const snapshotToArray_StudentCouse = snapshot => {
+  let returnArr = [];
+
+  snapshot.forEach(childSnapshot => {
+      let item = childSnapshot.val();
+      item.id = childSnapshot.key;
+     // console.log("course own id" + item.ownerid);
+     // console.log("current user: " + firebase.auth().currentUser.uid);
+      if (item.student_id == firebase.auth().currentUser.uid){
+          returnArr.push(item);
+      }
+  });
+
+  return returnArr;
 }
--- findTutor/src/app/tutor-orders/tutor-orders.page.ts
+++ findTutor/src/app/tutor-orders/tutor-orders.page.ts
@@ -11,23 +11,35 @@ import { Events } from '@ionic/angular';
 })
 export class TutorOrdersPage implements OnInit {
  
-  tutor_orders=[];
+  //tutor_orders=[];
   current_user = firebase.auth().currentUser;
+
+  ordersdb = firebase.database().ref('orders/');
+  tutor_orders: Array<any> =[];
   
   constructor(
     private router: Router,
     public itemService : ItemService,
     public events: Events
   ) { 
-    var self = this;
-    events.subscribe('dataloaded', (time) => {
-      // user and time are the same arguments passed in `events.publish(user, time)`
-      console.log('data load time:', time);
-      self.tutor_orders = this.itemService.getTutorOrders();
-      console.log("courses loaded: " + self.tutor_orders);
+    // var self = this;
+    // events.subscribe('dataloaded', (time) => {
+    //   // user and time are the same arguments passed in `events.publish(user, time)`
+    //   console.log('data load time:', time);
+    //   self.tutor_orders = self.itemService.getTutorOrders();
+    //   console.log("coursesssss loaded: " + self.tutor_orders);
+    //   });
+    //   // load orders from DB when construct the page
+    //   self.tutor_orders  = self.itemService.getTutorOrders();
+
+      this.ordersdb.on('value', resp => {
+        this.tutor_orders = [];
+        this.tutor_orders = snapshotToArray_TutorOrders(resp);
+        console.log(this.tutor_orders.length+" courses loaded");
+        console.log(this.tutor_orders);
+  
+        this.events.publish('dataloaded',Date.now());
       });
-      // load orders from DB when construct the page
-      this.tutor_orders  = this.itemService.getTutorOrders();
   }
 
   ngOnInit() {
@@ -37,15 +49,15 @@ export class TutorOrdersPage implements OnInit {
     // if(this.tutor_orders  != undefined){
     //       console.log("couse length" + this.tutor_orders.length);
     // }
-    var self = this;
-    this.events.subscribe('dataloaded', (time) => {
-      // user and time are the same arguments passed in `events.publish(user, time)`
-      console.log('data load time:', time);
-      self.tutor_orders = this.itemService.getTutorOrders();
-      console.log("courses loaded: " + self.tutor_orders);
-    });
-    // load orders from DB when construct the page
-    this.tutor_orders  = this.itemService.getTutorOrders();
+    // var self = this;
+    // this.events.subscribe('dataloaded', (time) => {
+    //   // user and time are the same arguments passed in `events.publish(user, time)`
+    //   console.log('data load time:', time);
+    //   self.tutor_orders = this.itemService.getTutorOrders();
+    //   console.log("courses loaded: " + self.tutor_orders);
+    // });
+    // // load orders from DB when construct the page
+    // self.tutor_orders  = this.itemService.getTutorOrders();
   }
 
   updateOrderAccept(course){
@@ -69,4 +81,20 @@ export class TutorOrdersPage implements OnInit {
     this.router.navigate(["/tutor-course-order-detail", course]);
   }
 
+}
+
+export const snapshotToArray_TutorOrders = snapshot => {
+  let returnArr = [];
+
+  snapshot.forEach(childSnapshot => {
+      let item = childSnapshot.val();
+      item.id = childSnapshot.key;
+     // console.log("course own id" + item.ownerid);
+     // console.log("current user: " + firebase.auth().currentUser.uid);
+      if (item.tutor_id == firebase.auth().currentUser.uid){
+          returnArr.push(item);
+      }
+  });
+
+  return returnArr;
 }
